526014745d339f47d9f8e45e8b86c6a647ab88eb
Ravi Shanth <46260788+ravishanth@users.noreply.github.com>
2023-06-09 11:37:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,14 +53,6 @@ streamlit.write('The user entered ', fruit_choice)
 #streamlit.stop()
 
 #-----------------------------------------------------------------
-
-
-
-'''my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from_steamlit')")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()'''
 streamlit.header("The fruit list contains")
 
 #snowflake related functions
